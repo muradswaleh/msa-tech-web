1226844cc71556112d36393e0d854ce364877188
Murad Swaleh <muradswaleh@gmail.com>
2013-07-18 18:10:27 +0300

    uploaded files

---
+++ about.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MombasaTech Community &middot; Welcome</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="MombasaTech Community Website Portal">
+    <meta name="author" content="Murad Swaleh">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/base.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+  </head>
+
+  <body>
+
+
+	<!-- Header  Logo and Social Media
+    ================================================== -->
+	<div class="header">
+		<div class="container marketing">
+			<div class="">
+				<a href="index.html"><img src="img/logo.png" alt="Logo" title="MombasaTech Community - Home" /></a>
+			
+				<div class="pull-right">
+				<a href="#" title="Member login" >Login</a> | 
+				<a href="#" title="Sign Up" >Sign Up</a>
+			  	</div>
+			
+			</div>
+		</div>
+		
+		
+		<div class="container marketing">	
+			<ul class="nav nav-pills pull-left">
+					<li><a href="index.html">Home</a></li>
+					<li class="active"><a href="about.html">About</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Forums</a></li>
+					<li><a href="#">Members</a></li>
+					<li><a href="#">Gallery</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contacts</a></li>
+			</ul>
+					
+			<div class="pull-right">
+				<a href="#" title="Follow us on Twitter" ><img class="mt-social" src="img/twitter.png" alt="MsaTech Twitter" height="30" width="30" /></a> 
+				<a href="#" title="Follow us on Facebook" ><img class="mt-social" src="img/facebook.png" alt="MsaTech Facebook" height="30" width="30" /></a>
+				<a href="#" title="Follow us on Google+" ><img class="mt-social" src="img/google+.png" alt="MsaTech Google+" height="30" width="30" /></a> 
+			</div>	
+		</div>
+			
+		
+	</div>
+	
+	
+	
+	<!-- MsaTech Intro
+    ================================================== -->
+	<div class="container">
+		<div class="row">
+			<div class="span12"><h2>About Us</h2></div>
+		</div>
+	</div>
+	
+    <!-- Home Menu
+    ================================================== -->
+
+     <div class="mt-menu">
+	  <div class="container marketing">
+	  <div class="mt-body">
+	  <div class="row">
+        <div class="span8">
+		 <div class="mt-article"> 	
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam ornare faucibus. Sed in pulvinar augue. Mauris faucibus varius lorem, iaculis convallis risus accumsan vel. Cras vestibulum, nibh vitae ornare congue, tortor mauris faucibus arcu, nec varius sem tellus vel lorem. Nulla elit nisl, varius et pretium eu, tristique at lacus. Duis imperdiet ultrices blandit. Curabitur metus odio, posuere vitae varius eu, lobortis non massa. Vestibulum mattis congue feugiat. Nunc ultrices arcu malesuada odio vestibulum nec dignissim neque consectetur. Duis eu egestas tortor. Vivamus rhoncus odio a enim imperdiet gravida.</p>
+		  <p>Aliquam id massa nibh. Etiam elementum, nisl a scelerisque adipiscing, velit erat adipiscing odio, nec adipiscing dolor tortor sit amet libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Sed lectus quam, suscipit id porta at, mattis in ligula. Sed fringilla molestie felis, id convallis neque cursus vel. Aliquam vitae lectus nisi.</p>
+		  <p>Phasellus imperdiet ullamcorper sagittis. Ut in nisl tellus. Aenean et quam in est iaculis vehicula eu vel felis. Morbi dignissim interdum massa quis lobortis. Nulla sit amet pharetra tellus. Quisque et diam est, quis egestas nisl. Cras neque magna, bibendum id ullamcorper ac, convallis eu augue. Donec diam nisi, molestie vitae luctus id, varius ut massa. Pellentesque in lacus justo. Integer congue hendrerit imperdiet. Mauris id erat eu sapien porta bibendum. In nisl nisi, sagittis id aliquam nec, vulputate accumsan sem. Cras auctor tincidunt auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam nisl elit, porttitor eget pellentesque sed, aliquet vel mauris.</p>
+		  <p>Ut tincidunt magna vel turpis rutrum auctor feugiat magna cursus. Etiam at augue arcu, sed tempor ipsum. Morbi viverra, sapien eleifend scelerisque lacinia, nisl urna pulvinar lorem, a adipiscing orci urna et velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam consequat tempus blandit. Nam posuere ipsum eget lorem faucibus fringilla. Aliquam at convallis quam. Nam posuere lectus in odio lacinia ullamcorper. Donec placerat, lectus in cursus sodales, tortor nulla vulputate erat, at vestibulum tortor leo in odio.</p>
+		  <p>Morbi sed mollis tellus. Sed non libero a magna sodales pellentesque ut eu lectus. Donec accumsan nunc id neque consectetur molestie. Nulla ac placerat tellus. Sed nec consequat augue. Maecenas vulputate vehicula ipsum. Curabitur ut tortor in libero convallis consequat at ac lorem. Quisque at suscipit sem. Aliquam quis interdum ligula. Aliquam vulputate commodo purus a sodales. In in orci tellus, vitae pellentesque nibh. Nulla at tellus quis nisl placerat iaculis. Nullam dui quam, mollis luctus facilisis ut, tincidunt ac urna. Sed elementum ante vitae leo pharetra sodales. Duis dolor enim, sollicitudin ac posuere condimentum, dictum eget augue.</p> 
+		 </div> 
+        </div><!-- /.span8 -->
+        
+		<div class="span4 mt-padding">
+          <div class="mt-sidebar"><img class="home-menu" src="img/events.png"></div>
+        </div><!-- /.span4 -->
+        
+      </div><!-- /.row -->
+	  </div>	  
+	  </div> <!-- /.container -->
+	 </div>	
+	 
+	<div class="container marketing">
+
+      <!-- START THE FEATURETTES -->
+	  <div class="featured">	
+      <div class="row">
+		<div class="span6">
+			<h3 class="featured-hd">About MombasaTech Community</h3>
+			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+
+		<div class="span6">
+			<h3 class="featured-hd">Membership and Benefits</h3>
+			<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+	  </div>
+	  </div>
+	  
+	  <hr>
+	  
+	</div>
+	
+	
+
+	<div class="footer">
+	<div class="container marketing">
+      <!-- FOOTER -->
+      <footer>
+	   <!-- START THE SPONSERS -->
+		<div class="row">
+			<div class="span3"><img src="img/browser-icon-firefox.png" alt="Mozilla" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-chrome.png" alt="Chrome" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-firefox.png" alt="Mozilla" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-chrome.png" alt="Chrome" height="150" width="150" /></div>
+		</div>
+		<hr>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2012 MombasaTech Community. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+    
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,212 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MombasaTech Community &middot; Welcome</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="MombasaTech Community Website Portal">
+    <meta name="author" content="Murad Swaleh">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/base.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+  </head>
+
+  <body>
+
+
+	<!-- Header  Logo and Social Media
+    ================================================== -->
+	<div class="header">
+		<div class="container marketing">
+			<div class="">
+				<a href="index.html"><img src="img/logo.png" alt="Logo" title="MombasaTech Community - Home" /></a>
+			
+				<div class="pull-right">
+				<a href="#" title="Member login" >Login</a> | 
+				<a href="#" title="Sign Up" >Sign Up</a>
+			  	</div>
+			
+			</div>
+		</div>
+		
+		<div class="container marketing">
+				<ul class="nav nav-pills pull-left">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Forums</a></li>
+					<li><a href="#">Members</a></li>
+					<li><a href="#">Gallery</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contacts</a></li>
+				</ul>
+			
+				<div class="pull-right">
+				<a href="#" title="Follow us on Twitter" ><img src="img/twitter.png" alt="MsaTech Twitter" height="30" width="30" /></a> 
+				<a href="#" title="Follow us on Facebook" ><img src="img/facebook.png" alt="MsaTech Facebook" height="30" width="30" /></a>
+				<a href="#" title="Follow us on Google+" ><img src="img/google+.png" alt="MsaTech Google+" height="30" width="30" /></a> 
+			  	</div>
+		</div>		
+	</div>
+	
+	<!-- MsaTech Intro
+    ================================================== -->
+	<div class="container">
+				<div id="myCarousel" class="carousel slide">
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+						<div class="active item"><img src="img/slide-01.jpg" alt="" /></div>
+						<div class="item"><img src="img/slide-01.jpg" alt="" /></div>
+						<div class="item"><img src="img/slide-01.jpg" alt="" /></div>
+					</div>
+					<!-- Carousel nav -->
+					<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+					<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+				</div>
+	</div>
+	
+    <!-- Home Menu
+    ================================================== -->
+
+     <div class="mt-menu">
+	  <div class="container marketing">
+	  <div class="mt-body">
+	  <div class="row">
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/about.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/events.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+         <a href="#"><img class="home-menu" src="img/members.png"></a>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+	  
+	  <hr>
+	  
+	  <div class="row">
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/mentors.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/blog.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+         <a href="#"><img class="home-menu" src="img/groups-forums.png"></a>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+	  
+	  <hr>
+	  
+	  <div class="row">
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/gallery.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <a href="#"><img class="home-menu" src="img/faqs.png"></a>
+        </div><!-- /.span4 -->
+        <div class="span4">
+         <a href="#"><img class="home-menu" src="img/contacts.png"></a>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+	  
+	  </div> <!-- MenuBG -->
+	  </div> <!-- /.container -->
+	 </div>	
+	 
+	<div class="container marketing">
+
+      <!-- START THE FEATURETTES -->
+	  <div class="featured">	
+      <div class="row">
+		<div class="span4">
+			<h3 class="featured-hd">Upcoming Events</h3>
+			<ul class="">
+			<li>Donec ullamcorper nulla non metus auctor fringilla.</li>
+			<li>Vestibulum id ligula porta felis euismod semper. </li>
+			<li>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</li>
+			<li>Fusce dapibus, tellus ac cursus commodo.</li>
+			</ul>
+		</div>
+		<div class="span4">
+			<h3 class="featured-hd">Membership</h3>
+			<p>Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+		<div class="span4">
+			<h3 class="featured-hd">Projects</h3>
+			<p>Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+		</div>
+	  </div>
+	  </div>
+	  
+	  <hr>
+	  	  
+	</div>
+	
+	
+
+	<div class="footer">
+	<div class="container marketing">
+      <!-- FOOTER -->
+      <footer>
+	   <!-- START THE SPONSERS -->
+		<div class="row">
+			<div class="span3"><img src="img/browser-icon-firefox.png" alt="Mozilla" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-chrome.png" alt="Chrome" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-firefox.png" alt="Mozilla" height="150" width="150" /></div>
+			<div class="span3"><img src="img/browser-icon-chrome.png" alt="Chrome" height="150" width="150" /></div>
+		</div>
+		
+		<hr> 
+		
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2012 MombasaTech Community. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+	</div>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+  </body>
+</html>
